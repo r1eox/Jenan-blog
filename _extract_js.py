897a6df3e9a94289d89c6044c96d9489fe5ca61e
جنان بيز <info@jenanbiz.sa>
2026-04-23 16:36:13 +0300

    إصلاح شامل: دمج دالة erpRenderReports المقسومة وإضافة بطاقة KPI رابعة وتنظيف ملفات الفحص

--- _extract_js.py
+++
@@ -1,8 +0,0 @@
-import re, sys
-with open('dashboard.html', encoding='utf-8') as f:
-    html = f.read()
-scripts = re.findall(r'<script[^>]*>(.*?)</script>', html, re.DOTALL)
-js = '\n'.join(scripts)
-with open('_check.js', 'w', encoding='utf-8') as f:
-    f.write(js)
-print('extracted', len(scripts), 'scripts, total chars:', len(js))
